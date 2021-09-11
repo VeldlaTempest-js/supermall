839837e76462fd16a79c2bf0036ea0623e54f63a
Veldla <2539078553@qq.com>
2021-09-11 22:22:13 +0800

    首页开发

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="tab-control">
-    <div v-for="item in titles"
-          class="tab-control-item">
+    <div v-for="(item, index) in titles"
+          class="tab-control-item"
+          :class="{active: index === currentIndex}"
+          @click="itemClick(index)">
       <span>{{ item }}</span>
     </div>
   </div>
@@ -16,6 +18,16 @@ export default {
       default() {
         return [];
       }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
     }
   }
 }
@@ -32,5 +44,14 @@ export default {
   }
   .tab-control-item {
     flex: 1;
+  }
+  .tab-control-item span {
+    padding: 5px;
+  }
+  .active {
+    color: var(--color-high-text);
+  }
+  .active span {
+    border-bottom: 3px solid var(--color-tint);
   }
 </style>
